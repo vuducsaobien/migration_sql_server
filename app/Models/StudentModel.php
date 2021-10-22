3c434db0c28416cba3bd10026b116851516634bc
ducvv <ducvv@mcrew-tech.com>
2021-10-23 06:29:19 +0700

    Migration 1 table Student

---
+++ app/Models/StudentModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StudentModel extends Model
+{
+    protected $table      = 'student';
+    public    $timestamps = false;
+}
---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\StudentModel as MainModel;
+
+class StudentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(MainModel::class, 10)->create();
+    }
+}
