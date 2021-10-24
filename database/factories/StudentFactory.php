fde8b039ff2441f83689ddf96aa9ed14b10d511b
ducvv <ducvv@mcrew-tech.com>
2021-10-24 16:17:35 +0700

    Migration 1 table Update

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -6,9 +6,71 @@ use App\Models\StudentModel as MainModel;
 use Faker\Generator as Faker;
 
 $factory->define(MainModel::class, function (Faker $faker) {
+
+    $string_1 = 'Alo';
+
     return [
-        'name'  => $faker->name,
-        // 'email' => $faker->unique()->safeEmail,
-        // 'phone' => $faker->phoneNumber,
+
+        // 01.String
+        'string_1'  => $faker->name,
+        'string_2'  => $faker->regexify('[A-Za-z0-9]{5}'),
+        'string_3'  => $faker->regexify('[A-Za-z0-9]{5} Hello'),
+        'string_4'  => $faker->randomElement(["foo $string_1", "bar $string_1", 'baz']),
+        'string_5'  => $faker->bothify($string = '## ??'),
+        'string_6'  => strtoupper( $faker->bothify( $string = '## ??' ) ),
+        'string_7'  => $faker->unique()->safeEmail,
+        'string_8'  => $faker->email,
+        'string_9'  => $faker->phoneNumber,
+        'string_10' => $faker->numerify('###-###-####'),
+        'string_11' => $faker->numerify('##########'),
+        
+        // 02. Number
+        'number_3'  => $faker->numberBetween(2, 5),
+        'number_4'  => $faker->unique()->numberBetween(2, 15),
+        'number_5'  => $faker->numberBetween(-2, 3),
+        'number_6'  => $faker->randomDigit(),
+        'number_7'  => $faker->randomDigitNotNull(),
+        'number_8'  => $faker->randomNumber(),
+        'number_9'  => $faker->randomFloat(),
+        'number_10' => $faker->randomFloat(4, 0, 10),
+        'number_11' => $faker->randomFloat(1, 0, 13) * 1000,
+
+        // 03. Date Time
+        'date_time_1'  => $faker->dateTime(),
+        'date_time_2'  => $faker->dateTime(),
+        'date_time_3'  => $faker->iso8601(),
+        'date_time_4'  => $faker->date(),
+        'date_time_5'  => $faker->date('Y_m_d'),
+        'date_time_6'  => $faker->date('Ymd'),
+        'date_time_7'  => $faker->time(),
+        'date_time_8'  => $faker->time('H_i_s'),
+        'date_time_9'  => $faker->time('His'),
+        'date_time_10' => $faker->dateTimeBetween(),
+        'date_time_11' => $faker->dateTimeBetween('-1 week', '+1 week'),
+        'date_time_12' => $faker->dateTimeBetween('-1 days', '+1 days'),
+        'date_time_13' => $faker->dateTimeThisDecade(),
+        'date_time_14' => $faker->dateTimeThisYear(),
+        'date_time_15' => $faker->dateTimeThisMonth(),
+        'date_time_16' => $faker->amPm(),
+        'date_time_17' => $faker->amPm('+ 2 weeks'),
+        'date_time_18' => $faker->dayOfMonth(),
+        'date_time_19' => $faker->dayOfWeek(),
+        'date_time_20' => $faker->month(),
+        'date_time_21' => $faker->monthName(),
+        'date_time_22' => $faker->year(),
+
+        // 04. Exten
+        'ext_1' => $faker->boolean,
+
+        'ext_2' => implode( $faker->randomElements([
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'
+        ], 5) ),
+
+        'ext_3' => implode( $faker->randomElements([
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'
+        ], 6, true) ),
+
+        'ext_4' => $faker->imageUrl($width = 200, $height = 200),
+
     ];
 });
--- database/migrations/2021_10_22_144321_student.php
+++ database/migrations/2021_10_22_144321_student.php
@@ -17,10 +17,57 @@ class Student extends Migration
     {
         Schema::create($this->table_db, function (Blueprint $table) {
             $table->id();
-            // $table->bigIncrements('id');
-            $table->string('name');
-            // $table->string('email')->unique();
-            // $table->string('phone');
+
+            $table->string('string_1');
+            $table->string('string_2', 5);
+            $table->string('string_3', 11);
+            $table->string('string_4', 10);
+            $table->string('string_5');
+            $table->string('string_6');
+            $table->string('string_7')->unique();
+            $table->string('string_8');
+            $table->string('string_9');
+            $table->string('string_10');
+            $table->string('string_11');
+
+            $table->integer('number_3');
+            $table->integer('number_4');
+            $table->integer('number_5');
+            $table->integer('number_6');
+            $table->integer('number_7');
+            $table->integer('number_8');
+            $table->float('number_9');
+            $table->float('number_10');
+            $table->float('number_11');
+
+            $table->dateTime('date_time_1');
+            $table->dateTime('date_time_2', 6);
+            $table->string('date_time_3');
+            $table->string('date_time_4');
+            $table->string('date_time_5');
+            $table->string('date_time_6');
+            $table->string('date_time_7');
+            $table->string('date_time_8');
+            $table->string('date_time_9');
+            $table->dateTime('date_time_10');
+            $table->dateTime('date_time_11');
+            $table->dateTime('date_time_12');
+            $table->dateTime('date_time_13');
+            $table->dateTime('date_time_14');
+            $table->dateTime('date_time_15');
+            $table->string('date_time_16');
+            $table->string('date_time_17');
+            $table->string('date_time_18');
+            $table->string('date_time_19');
+            $table->string('date_time_20');
+            $table->string('date_time_21');
+            $table->string('date_time_22');
+
+            $table->boolean('ext_1');
+            $table->string('ext_2');
+            $table->string('ext_3');
+            $table->string('ext_4');
+
             // $table->timestamps();
         });
     }
